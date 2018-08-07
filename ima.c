e417db2caa35fc266dcbfbb93627e8f01aa80590
Alther01 <41448110+Alther01@users.noreply.github.com>
2018-08-07 11:15:17 -0300

    Create ima.c

---
+++ ima.c
@@ -0,0 +1,27 @@
+#include  <stdio.h>
+
+comparador()
+
+
+int leitora(int n, int ultimo)
+{
+	int n;
+
+	scanf("%d", &n);
+
+	/*if(n <= 0)
+		printf("groups: 0, biggest: 0\n");
+	if(n == 1)
+		printf("groups: 1, biggest: 1\n");*/
+
+	comparador(n)
+}
+
+
+int main()
+{
+	
+	leitora(n)
+	
+	return 0;
+}
