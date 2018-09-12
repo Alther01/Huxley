c4c3d659c08a0d2a61ad0e2a7bc3af9bc5524f4d
Alther01 <41448110+Alther01@users.noreply.github.com>
2018-09-12 17:04:18 -0300

    Create gigante.c

---
+++ gigante.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+	void somadora(int size_a,int size_b,char string_a,char string_b, int i){
+		if(i == size_a - 1){
+			return;
+		}
+		if(i >= size_b){
+			string_b[i] = 0; 
+		}
+		int soma = string_a[i] + string_b[i];
+		if(soma > 9){
+			soma = (string_a[i] + string_b[i]) % 10;
+			string_a[i + 1] += 1;
+		}
+		i++;
+		somadora(size_a, size_b, string_a, string_b, i);
+		printf("%d\n", soma);
+	}
+	void inversora(char string[], int size, int i){
+		if(i == size/2){
+			return;
+		}
+		int aux = string[i] - '0';
+		string[i] = string[size - i - 1 ] - '0';
+		string[size - i - 1] = aux;
+		printf("%s\n", string);
+		inversora(string, size, i + 1);
+	}
+	int armazenadora(char string_aux[], int i){
+		scanf("%s", string_aux);
+		int size = strlen(string_aux);
+		return size;
+	}
+	int main(){
+		char string_a[10000];
+		char string_b[10000];
+		int size_a = armazenadora(string_a, 0);
+		int size_b = armazenadora(string_b, 0);
+		inversora(string_a, size_a, 0);
+		inversora(string_b, size_b, 0);
+		if(size_a >= size_b){
+			somadora(size_a, size_b, string_a, string_b, 0);
+		}
+		/*if(size_b > size_a){
+			somadora(size_a, size_b, string_a, string_b);
+		}*/		
+		return 0;
+	}
+	/*0 1 2 3 -- 4
+	a b c d
+	int aux = string[i];
+	string[i] = string[size - i - 1];
+	string[size - i - 1] = aux;*/
