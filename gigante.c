fa4c416d93a3d6138c396f7fb1efd2aed4927734
Alther01 <41448110+Alther01@users.noreply.github.com>
2018-09-13 11:18:13 -0300

    Update gigante.c

--- gigante.c
+++ gigante.c
@@ -52,3 +52,39 @@
 	int aux = string[i];
 	string[i] = string[size - i - 1];
 	string[size - i - 1] = aux;*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define tam 800
+	void somadora(int size_a,int size_b,char string_a,char string_b, int i){
+		
+	}
+	void inteirona(int size, char string[], int i, int inteiro[]){
+		if(i == size){
+			return;
+		}
+		inteiro[tam - size + i] = string[i] - '0';
+		inteirona(size, string, i + 1, inteiro);
+	}
+	int armazenadora(char string_aux[], int i){
+		scanf("%s", string_aux);
+		int size = strlen(string_aux);
+		return size;
+	}
+	int main(){
+		int inteiro_a[tam];
+		int inteiro_b[tam];
+		char string_a[tam];
+		char string_b[tam];
+		int size_a = armazenadora(string_a, 0);
+		int size_b = armazenadora(string_b, 0);
+		//inversora(string_a, size_a, 0);
+		//inversora(string_b, size_b, 0);
+		inteirona(size, string_a, 0, inteiro_a);
+		inteirona(size, string_b, 0, inteiro_b);
+		somadora(size_a, size_b, string_a, string_b, 0);
+		}
+		return 0;
+	}
